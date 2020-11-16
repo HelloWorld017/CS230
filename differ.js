8f48517556e4fdac1d97672bbc84fbbc256a3aa3
Khinenw <deu07115@gmail.com>
2020-11-16 21:18:14 +0900

    test(lab5): Update diff tool

--- differ.js
+++ differ.js
@@ -1,18 +1,61 @@
+const { exec, spawn } = require('child_process');
 const { promises: fsp } = require('fs');
 
 const replacer = str =>
 	str
-		.replace(/^\d+\s*pts\/\d+\s*([A-Z]+)\s*\d+:\d+ .*$/gm, '$1') // Replace ps table
-		.replace(/^(Job )?[(\d+)] \(\d+\)/gm, '$1[$2]') // Replace Job [jid] (pid), [jid] (pid)
-		.replace(/^\.\/sdriver\.pl -t trace\(d+)\.txt -s .*$/gm, 'Test $1') // Replace ./sdriver.pl ...
+		.replace(/^\s*\d+\s*(?:pts\/|tty)\d+\s*([A-Za-z+]+)\s*\d+:\d+\s*(.*)$/gm, (match, p1, p2) => {
+			const p2r = p2.replace(/rtest/g, 'test').replace(/tshref/g, 'tsh');
+
+			return `${p1}: ${p2r}`;
+		}) // Replace ps table
+		.replace(/^(Job )?\[(\d+)\] \(\d+\)/gm, '$1[$2]') // Replace Job [jid] (pid), [jid] (pid)
+		.replace(/^\.\/sdriver\.pl -t trace(\d+)\.txt -s .*$/gm, 'Test $1') // Replace ./sdriver.pl ...
 		
 
+const execPromise = command => 
+	new Promise(resolve => {
+		exec(command, (err, stdout, stderr) => {
+			if (err) {
+				console.error("Differ Error!");
+				console.error(err);
+			}
+			
+			const output =
+				'======= STDOUT =======\n' +
+				`${stdout}\n` +
+				'======= OUTPUT =======\n' +
+				`${stderr}\n`;
+			
+			resolve(output);
+		});
+	});
+
+const testRun = async testPrefix => {
+	let output = '';
+	for (let i = 1; i <= 16; i++) {
+		const testName = `${testPrefix}${i.toString().padStart(2, '0')}`;
+		console.log(`Running ${testName}`);
+
+		output += await execPromise(`make ${testName}`);
+	}
+	
+	return output;
+};
+
 (async () => {
-	const tshOut = await fsp.readFile('./tsh.out', 'utf8');
-	const refOut = await fsp.readFile('./tshref.out', 'utf8');
+	const tshOut = replacer(await testRun('test'));
+	const refOut = replacer(await testRun('rtest'));
 	
-	await fsp.writeFile('./tsh.out', replacer(tshOut));
-	await fsp.writeFile('./tshref.out', replacer(refOut));
+	await fsp.writeFile('./tsh.out', tshOut);
+	await fsp.writeFile('./tshref.out', refOut);
 	
-	console.log('Done :D');
+	if (tshOut === refOut) {
+		console.log("Two output matches!");
+		return;
+	}
+	
+	spawn('vimdiff', ['./tsh.out', './tshref.out'], {
+		detached: true,
+		stdio: 'inherit'
+	});
 })();
