1c6e6567bd2d878ab7d930c81d51342073d1a63b
Khinenw <deu07115@gmail.com>
2020-12-10 17:15:59 +0900

    fix(lab7): Fix bugs in parser

--- proxy.c
+++ proxy.c
@@ -9,28 +9,12 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main(int argc, char** argv) {
-	struct sockaddr_in clientaddr;
-	int clientlen = sizeof(clientaddr);
-	pthread_t tid;
-	int listenfd = open_listenfd(argv[1]);
-	printf("Listening on %s...", argv[1]);
-
-	while (1) {
-		int* connfdp = malloc(sizeof(int));
-		*connfdp = accept(listenfd, (SA*) &clientaddr, &clientlen);
-		pthread_create(&tid, NULL, receive_thread, connfdp);
-	}
-
-	return 0;
-}
-
-int expect(char** ptr_ptr, const char* cmp) {
+unsigned expect(char** ptr_ptr, const char* cmp) {
 	char* start_ptr = *ptr_ptr;
-
 	if (start_ptr == NULL)
 		return 0;
 
+	size_t len = strlen(cmp);
 	if (!strncmp(start_ptr, cmp, len)) {
 		*ptr_ptr = start_ptr + len;
 		return 1;
@@ -53,7 +37,7 @@ int advance_while(char** ptr_ptr, const char* delimiter) {
 			return consume;
 		}
 
-		if (strncmp(*ptr_ptr, cmp, delim_len)) {
+		if (!strncmp(*ptr_ptr, delimiter, delim_len)) {
 			return consume;
 		}
 
@@ -69,18 +53,18 @@ int advance_till(char** ptr_ptr, const char* delimiter) {
 		return 0;
 
 	size_t delim_len = strlen(delimiter);
-	int consume;
+	int consume = 0;
 
 	while (1) {
 		if (**ptr_ptr == '\0')
 			return 0;
 
-		if (!strncmp(*ptr_ptr, cmp, delim_len)) {
+		if (!strncmp(*ptr_ptr, delimiter, delim_len)) {
 			return consume;
 		}
 
 		consume++;
-		*ptr_ptr++;
+		(*ptr_ptr)++;
 	}
 }
 
@@ -103,12 +87,13 @@ typedef struct {
 	char* value;
 } Header;
 
+#define MAX_HEADERS 128
 typedef struct {
 	int method;
 	char* host;
 	char* path;
-	char* version;
-	Header* headers[];
+	int version;
+	Header* headers;
 	unsigned header_length;
 	char* body;
 } Request;
@@ -137,26 +122,36 @@ int parse_version(char* version) {
 	return VERSION_UNKNOWN;
 }
 
-#define MAX_HEADERS 128
+void parse_url(char* url, char** host, char** path) {
+	char** ptr = &url;
+	if (expect(ptr, "http://")) {
+		*host = match(ptr, "/");
+	}
 
-int parse_header(rio_t rio, Header** headers, int* status) {
+	*path = *ptr;
+}
+
+int parse_header(rio_t* rio, Header** headers, int* status) {
 	Header* header = malloc(sizeof(Header) * MAX_HEADERS);
 	*headers = header;
 
 	for (int i = 0; i < MAX_HEADERS; i++) {
-		char[MAX_LINE] line;
-		if (rio_readlineb(rio, line, MAX_LINE) <= 0) {
+		char line_buffer[MAX_LINE];
+
+		if (rio_readlineb(rio, line_buffer, MAX_LINE) <= 0) {
 			return i;
 		}
+
+		char* line = line_buffer;
 
 		if (expect(&line, "\r\n"))
 			return i;
 
 		header->key = match(&line, ":");
-		expect(&line, " ");
+		expect(&line, ": ");
 		header->value = match(&line, "\r");
 
-		int parse_success = expect(&line, "\n");
+		unsigned parse_success = expect(&line, "\r\n");
 		if (!parse_success) {
 			*status = 400;
 			return i;
@@ -166,23 +161,24 @@ int parse_header(rio_t rio, Header** headers, int* status) {
 	}
 }
 
-Request* parse_request(rio_t rio, int* status) {
-	char[MAX_LINE] line;
-	if (rio_readlineb(rio, line, MAX_LINE) <= 0) {
+Request* parse_request(rio_t* rio, int* status) {
+	char line_buffer[MAX_LINE];
+	if (rio_readlineb(rio, line_buffer, MAX_LINE) <= 0) {
 		*status = 400;
 		return NULL;
 	}
 
+	char* line = line_buffer;
+
 	// Parse method, host, path and http version
-	int parse_success = 1;
+	unsigned parse_success = 1;
 	char* method = match(&line, " ");
 	parse_success &= expect(&line, " ");
-	parse_success &= expect(&line, "http://");
 
-	char* host = match(&line, "/");
-	char* path = match(&line, " ");
+	char* url = match(&line, " ");
+	parse_success &= expect(&line, " ");
 	char* version = match(&line, "\r");
-	parse_success &= expect(&line, "\n");
+	parse_success &= expect(&line, "\r\n");
 
 	if (!parse_success) {
 		*status = 400;
@@ -196,8 +192,7 @@ Request* parse_request(rio_t rio, int* status) {
 		return NULL;
 	}
 
-	request->host = host;
-	request->path = path;
+	parse_url(url, &request->host, &request->path);
 	request->version = parse_version(version);
 	if (request->version == VERSION_UNKNOWN) {
 		*status = 505;
@@ -213,20 +208,22 @@ Request* parse_request(rio_t rio, int* status) {
 	return request;
 }
 
-void dump_request(Request* req) {
+void dump_request(Request* req, int status) {
 	if (!req) {
-		printf("Failed to parse request!\n");
+		printf("Failed to parse request with status %d!\n", status);
 		return;
 	}
 
-	printf("Version: %s\n", req->version);
-	printf("Method: %s\n", req->method);
+	printf("======== HTTP REQUEST ========\n");
+	printf("Version: %d\n", req->version);
+	printf("Method: %d\n", req->method);
 	printf("Host: %s\n", req->host);
 	printf("Path: %s\n", req->path);
-	printf("Headers ========\n");
+	printf("Headers ------\n");
 	for (int i = 0; i < req->header_length; i++) {
-		printf("Key: %s, Value: %s\n", req->headers[i]->key, req->headers[i]->value);
+		printf("Key: %s, Value: %s\n", req->headers[i].key, req->headers[i].value);
 	}
+	printf("\n");
 }
 
 void* receive_thread(void* vargp) {
@@ -238,9 +235,25 @@ void* receive_thread(void* vargp) {
 	rio_readinitb(&rio, connfd);
 
 	int status;
-	Request* req = parse_request(rio, &status);
-	dump_request(req);
+	Request* req = parse_request(&rio, &status);
+	dump_request(req, status);
 	
 	close(connfd);
 	return NULL;
+}
+
+int main(int argc, char** argv) {
+	struct sockaddr_in clientaddr;
+	unsigned clientlen = sizeof(clientaddr);
+	pthread_t tid;
+	int listenfd = open_listenfd(argv[1]);
+	printf("Listening on %s...\n", argv[1]);
+
+	while (1) {
+		int* connfdp = malloc(sizeof(int));
+		*connfdp = accept(listenfd, (SA*) &clientaddr, &clientlen);
+		pthread_create(&tid, NULL, receive_thread, connfdp);
+	}
+
+	return 0;
 }
